ef298b12cbf52952223f7e473f79e107cb17b6d8
alumno <alumno@LIMSFLAB06-15D.system-uap.adm>
2017-08-28 21:54:21 -0500

    first commit

---
+++ src/s07ejercicio04/S07ejercicio04.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s07ejercicio04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class S07ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Creando objeto Scanner
+        Scanner input = new Scanner (System.in);
+        
+        // Mostrando mensaje al usuario
+        System.out.println("INGRESE NUMERO DE MINUTOS: ");
+        int minutes = input.nextInt();
+        
+        // Calcular el numero de años y dias
+        int years = minutes / 525600;
+        int days = (minutes % 525600) / 1440;
+        
+        // Mostrando resutados
+        System.out.println(minutes + " minutos son aproximadamente: " + years + " años " + days + " days ");
+    }
+    
+}
